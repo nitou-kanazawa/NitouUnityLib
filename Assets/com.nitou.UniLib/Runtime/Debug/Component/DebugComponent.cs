9b8728834803f509c1b27f26e04097bd6e51bcff
伊藤 夏輝(ＣＴ５) <nat-itoh@cec-ltd.co.jp>
2024-08-02 18:14:28 +0900

    RectTransformのDebug表示関連のコンポーネントを追加.

---
+++ Assets/com.nitou.UniLib/Runtime/Debug/Component/DebugComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace nitou.DebugScripts{
+
+    public abstract class DebugComponent<TComponent> : MonoBehaviour 
+        where TComponent : Component{
+        
+       
+    }
+}
