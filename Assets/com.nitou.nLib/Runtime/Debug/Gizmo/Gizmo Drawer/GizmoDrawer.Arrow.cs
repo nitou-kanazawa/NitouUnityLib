20d81d0ba132035f1969d74f9916a98fad29fc8a
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2024-08-04 11:13:41 +0900

    矢印のGizmo描画クラスを追加

--- Assets/com.nitou.nLib/Runtime/Debug/Gizmo/Gizmo Drawer/GizmoDrawer.Arrow.cs
+++ Assets/com.nitou.nLib/Runtime/Debug/Gizmo/Gizmo Drawer/GizmoDrawer.Arrow.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace nitou.DebugInternal{
-    internal static partial class GizmoDrawer{
+// [éQçl]
+//  github: MatthewMaker/DrawArrow.cs https://gist.github.com/MatthewMaker/5293052
+
+namespace nitou.DebugInternal {
+    internal static partial class GizmoDrawer {
 
         /// <summary>
         /// ñÓàÛ
@@ -17,7 +20,8 @@ namespace nitou.DebugInternal{
             private const int CIRCLE_SEGMENTS = 8;
 
 
-            /*
+            /// ----------------------------------------------------------------------------
+            #region ï`âÊÉÅÉ\ÉbÉh (Gizmo)
 
             /// <summary>
             /// à íuÅCï˚å¸ÇéwíËÇµÇƒñÓàÛÇï`âÊÇ∑ÇÈ
@@ -25,31 +29,31 @@ namespace nitou.DebugInternal{
             public static void DrawRayArrow(ArrowType type, Vector3 pos, Vector3 direction,
                 float arrowHeadLength = 0.08f, float arrowHeadAngle = 20.0f) {
 
-                // Arrow shaft
-                Gizmos.DrawRay(pos, direction);
-
                 // í∑Ç≥ÇOÇ»ÇÁèIóπ
                 if (direction == Vector3.zero) return;
 
+                // Arrow shaft
+                Gizmos.DrawRay(pos, direction);
+
                 // Arrow head
                 switch (type) {
-                    case ArrowType.Line:
-                        var (right, left) = CalcArrowHeadDirection(direction, arrowHeadAngle);
-                        Gizmos.DrawRay(pos + direction, right * arrowHeadLength);
-                        Gizmos.DrawRay(pos + direction, left * arrowHeadLength);
-                        break;
-
-                    case ArrowType.Solid:
-                        float radius = 0.025f;
-                        CylinderDrawer.DrawWireCone(
-                            MathUtil.PlaneType.XY,
-                            pos + direction,
-                            Quaternion.LookRotation(direction),
-                            radius,
-                            arrowHeadLength,
-                            CIRCLE_SEGMENTS
-                        );
-                        break;
+                    case ArrowType.Line: {
+                            var (right, left) = CalcArrowHeadDirection(direction, arrowHeadAngle);
+                            Gizmos.DrawRay(pos + direction, right * arrowHeadLength);
+                            Gizmos.DrawRay(pos + direction, left * arrowHeadLength);
+                            break;
+                        }
+                    case ArrowType.Solid: {
+                            float radius = 0.025f;
+                            Cylinder.DrawWireCone(
+                                PlaneType.XY,   // Å¶Zï˚å¸ÇäÓèÄÇ…âÒì]
+                                pos + direction,
+                                Quaternion.LookRotation(direction),
+                                radius,
+                                arrowHeadLength,
+                                CIRCLE_SEGMENTS);
+                            break;
+                        }
                 }
 
             }
@@ -60,39 +64,37 @@ namespace nitou.DebugInternal{
             public static void DrawLineArrow(ArrowType type, Vector3 from, Vector3 to,
                 float arrowHeadLength = 0.2f, float arrowHeadAngle = 20.0f) {
 
-                // Arrow shaft
-                Gizmos.DrawLine(from, to);
-
                 // í∑Ç≥ÇOÇ»ÇÁèIóπ
                 if (Mathf.Approximately(Vector3.Distance(from, to), 0f)) return;
+
+                // Arrow shaft
+                Gizmos.DrawLine(from, to);
 
                 // Arrow head
                 var direction = to - from;
                 switch (type) {
-                    case ArrowType.Line:
-                        var (right, left) = CalcArrowHeadDirection(direction, arrowHeadAngle);
-                        Gizmos.DrawRay(to, right * arrowHeadLength);
-                        Gizmos.DrawRay(to, left * arrowHeadLength);
-                        break;
-
-                    case ArrowType.Solid:
-                        float radius = 0.025f;
-                        CylinderDrawer.DrawWireCone(
-                            MathUtil.PlaneType.XY,
-                            from + direction,
-                            Quaternion.LookRotation(direction),
-                            radius,
-                            arrowHeadLength,
-                            CIRCLE_SEGMENTS
-                        );
-                        break;
+                    case ArrowType.Line: {
+                            var (right, left) = CalcArrowHeadDirection(direction, arrowHeadAngle);
+                            Gizmos.DrawRay(to, right * arrowHeadLength);
+                            Gizmos.DrawRay(to, left * arrowHeadLength);
+                            break;
+                        }
+                    case ArrowType.Solid: {
+                            float radius = 0.025f;
+                            Cylinder.DrawWireCone(
+                                PlaneType.XY,   // Å¶Zï˚å¸ÇäÓèÄÇ…âÒì]
+                                from + direction,
+                                Quaternion.LookRotation(direction),
+                                radius,
+                                arrowHeadLength,
+                                CIRCLE_SEGMENTS);
+                            break;
+                        }
                 }
 
 
             }
-
-
-            */
+            #endregion
 
 
             /// ----------------------------------------------------------------------------
