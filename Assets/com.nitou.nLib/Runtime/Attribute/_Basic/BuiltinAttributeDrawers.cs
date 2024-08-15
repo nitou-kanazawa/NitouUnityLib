7dfc320117c597b5ca713f8a1e33d9417c358110
nat-itoh <nat-itoh@cec-ltd.co.jp>
2024-08-15 18:50:58 +0900

    Title Attribute Drawerのバグを修正．

--- Assets/com.nitou.nLib/Runtime/Attribute/_Basic/BuiltinAttributeDrawers.cs
+++ Assets/com.nitou.nLib/Runtime/Attribute/_Basic/BuiltinAttributeDrawers.cs
@@ -67,12 +67,13 @@ namespace nitou.EditorScripts.Drawers {
 
     [CustomPropertyDrawer(typeof(TitleAttribute))]
     public class TitleDrawer : PropertyDrawer {
+        private const int SPACE_HEIGHT = 5;
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-            TitleAttribute titleAttribute = (TitleAttribute)attribute;
+            var titleAttribute = (TitleAttribute)attribute;
 
             // タイトルの高さ
-            Rect titlePosition = new Rect(position.x, position.y, position.width, EditorGUIUtility.singleLineHeight);
+            var titlePosition = position.SetHeight(EditorGUIUtility.singleLineHeight);
             EditorGUI.LabelField(titlePosition, titleAttribute.TitleText, EditorStyles.boldLabel);
 
             // 下線を描画
@@ -80,13 +81,17 @@ namespace nitou.EditorScripts.Drawers {
             EditorGUI.DrawRect(linePosition, Color.gray);
 
             // 元のフィールドを描画する位置を調整
-            Rect propertyPosition = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight + 3, position.width, position.height);
-            EditorGUI.PropertyField(propertyPosition, property, label, true);
+            Rect propertyPosition = new Rect(position.x, position.y + EditorGUIUtility.singleLineHeight + SPACE_HEIGHT, position.width, position.height);
+
+            // ここで新しいラベルを生成して使用
+            GUIContent propertyLabel = new GUIContent(property.displayName);
+            EditorGUI.PropertyField(propertyPosition, property, propertyLabel, true);
         }
+
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label) {
             // タイトル、下線、プロパティのスペースを考慮して高さを計算
-            return EditorGUIUtility.singleLineHeight + 4 + base.GetPropertyHeight(property, label);
+            return EditorGUIUtility.singleLineHeight + SPACE_HEIGHT + base.GetPropertyHeight(property, label);
         }
     }
     #endregion
