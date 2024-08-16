bf2fce989349f43bece7f3d5afd27fb9cf82f4e6
nat-itoh <nat-itoh@cec-ltd.co.jp>
2024-08-16 18:07:52 +0900

    ScrollRectのデバッグコンポーネントを追加．

--- Assets/Develop/ScrollViewHighlighter.cs
+++ Assets/Develop/ScrollViewHighlighter.cs
@@ -6,27 +6,29 @@ using nitou.Inspector;
 namespace Project {
     public class ScrollViewHighlighter : MonoBehaviour {
 
-        [SerializeField] ScrollRect _scrollView;
-        [SerializeField] RectTransform _selectedItem;
-        [SerializeField] ScrollVarTick _icon;
+        [SerializeField, Indent] ScrollRect _scrollView;
+        [SerializeField, Indent] RectTransform _selectedItem;
+        [SerializeField, Indent] ScrollVarTick _icon;
 
         [Title("Info")]
-        public float normalizedY = 0;
+
+        [ReadOnly]
+        [SerializeField, Indent] float normalizedY = 0;
 
 
         void Update() {
+
+            var itemPosition = _selectedItem.GetWorldCenterPosition();
+            
             // Content内の相対位置
-            var selectedCenter = _selectedItem.GetWorldCenterPosition();
-            Debug.Log($"selectedCenter :{selectedCenter}");
-
-            // ※Content SizeFitterのサイズは0になる
-            var contentRect = _scrollView.content.GetWorldRect();
-            Debug.Log($"contentRect :{contentRect}");
-
+            var contentRect = _scrollView.content.GetWorldRect();   // ※開始時のContent SizeFitterのサイズは0になるので注意
             if (contentRect.size == Vector2.zero) return;
+            var relativePos = RectUtil.GetRelativePosition(itemPosition, contentRect);
 
-            var relativePos = RectUtil.GetRelativePosition(selectedCenter, contentRect);
-            Debug.Log($"relativePos :{relativePos}");
+            //Debug.Log($"contentRect :{contentRect}");
+
+
+            //Debug.Log($"relativePos :{relativePos}");
             normalizedY = relativePos.y;
 
             _icon.Rate = normalizedY;
---
+++ Assets/com.nitou.nLib/Runtime/Debug/Component/ScrollRectDebugger.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace nitou.DebugInternal{
+
+    /// <summary>
+    /// <see cref="ScrollRect"/>の各プロパティを可視化するためのデバッグ用コンポーネント
+    /// </summary>
+    internal class ScrollRectDebugger : DebugComponent<ScrollRect>{
+
+
+    }
+}
