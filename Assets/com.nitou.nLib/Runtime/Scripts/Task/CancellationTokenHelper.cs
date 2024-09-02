fd89b4e6574ed45d17d34e204f5a130f67e9f4d0
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2024-09-02 23:56:49 +0900

    Task関連のUtilクラスを追加．

---
+++ Assets/com.nitou.nLib/Runtime/Scripts/Task/CancellationTokenHelper.cs
@@ -0,0 +1,60 @@
+using System.Threading;
+
+// [参考]
+//  Zenn: UniRx/UniTask大全 https://zenn.dev/tmb/articles/e4fb3fe350852f
+
+namespace nitou {
+
+    /// <summary>
+    /// CancellationTokenSourceインスタンスを入れ替えて繰り返し使用するためのラッパー
+    /// </summary>
+    public class CancellationTokenHelper {
+        
+        /// <summary>
+        /// ソース
+        /// </summary>
+        public CancellationTokenSource Cts { get; private set; }
+
+        /// <summary> 
+        /// トークン 
+        /// </summary>
+        public CancellationToken Token => Cts.Token;
+
+
+        /// ----------------------------------------------------------------------------
+        // Public Method
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public CancellationTokenHelper() {
+            Reset();
+        }
+
+        /// <summary>
+        /// デストラクタ
+        /// </summary>
+        ~CancellationTokenHelper() {
+            Dispose();
+        }
+
+        /// <summary>
+        /// リセット
+        /// </summary>
+        public void Reset() {
+            Dispose();
+            Cts = new CancellationTokenSource();
+        }
+
+        /// <summary>
+        /// 破棄
+        /// </summary>
+        public void Dispose() {
+            if (Cts != null) {
+                Cts.Cancel();
+                Cts.Dispose();
+                Cts = null;
+            }
+        }
+    }
+}
