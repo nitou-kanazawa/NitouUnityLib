ff943ef12c22b2d347f07080b536568bd739223b
nitou-kanazawa <121334451+nitou-kanazawa@users.noreply.github.com>
2024-12-15 15:59:05 +0900

    ディレクトリ整理．

---
+++ Assets/com.nitou.nModules/Core Modules/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Nitou.Modules.Editor")]
+[assembly: InternalsVisibleTo("Nitou.Modules.Test_Editor")]
+[assembly: InternalsVisibleTo("Nitou.Modules.Test_Runtime")]
